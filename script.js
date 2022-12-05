cfa31c18ed40c21cb5b080c6e9465cf09f264747
Nicolas Milhem <nicolasmilhem@gmail.com>
2022-12-05 14:43:05 +0100

    Correction couleur et signe de variation 7j-30j-200j

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const forms = document.querySelectorAll('form');
 const btnReset = document.querySelector('.btn-reset');
-const priceOneDay = document.querySelector('.price-oneday');
 const priceOneWeek = document.querySelector('.price-oneweek');
 const priceOneMonth = document.querySelector('.price-onemonth');
 const price200days = document.querySelector('.price-200days');
@@ -26,9 +25,11 @@ const getCurrencies = async () => {
     try {
         const response = await axios.get(`https://api.coingecko.com/api/v3/coins/ethereum?localization=false&tickers=false&market_data=true&community_data=false&developer_data=false&sparkline=false`);
 
+        const marketData = response.data.market_data;
+
         // Récupération taux de l'ETH en Euro
 
-        const ethRateInEUR = response.data.market_data.current_price.eur;
+        const ethRateInEUR = marketData.current_price.eur;
 
         // Affichage prix d'1 ETH (par défaut)
 
@@ -37,24 +38,30 @@ const getCurrencies = async () => {
 
         // Valeur d'un ETH
 
-        priceOneETH.innerText = `€ ${response.data.market_data.current_price.eur.toFixed(2)}`;
+        priceOneETH.innerText = `€ ${marketData.current_price.eur.toFixed(2)}`;
 
         // Variation du prix sur 24h
 
-        const variationPercentOneDay = response.data.market_data.price_change_percentage_24h_in_currency.eur.toFixed(2);
+        const variationPercentOneDay = marketData.price_change_percentage_24h_in_currency.eur.toFixed(2);
 
         priceOneETHVar.innerText = `${variationPercentOneDay > 0 ? "+" : ""}${variationPercentOneDay} %`;
         priceOneETHVar.style.backgroundColor = variationPercentOneDay > 0 ? "green" : "red";
 
-        priceOneETH24hValue.innerText = `Variation / 24h - Valeur : € ${(response.data.market_data.current_price.eur - response.data.market_data.price_change_24h_in_currency.eur).toFixed(2)}`;
+        priceOneETH24hValue.innerText = `Variation / 24h - Valeur : € ${(marketData.current_price.eur - marketData.price_change_24h_in_currency.eur).toFixed(2)}`;
 
         // Variation du prix sur 7, 30 et 200 jours
 
-        priceOneWeek.innerHTML = `€ ${((response.data.market_data.current_price.eur * 100) / (100 + response.data.market_data.price_change_percentage_7d_in_currency.eur)).toFixed(2)} <span>${response.data.market_data.price_change_percentage_7d_in_currency.eur.toFixed(2)}%</span>`;
+        priceOneWeek.innerHTML = `€ ${((marketData.current_price.eur * 100) / (100 + marketData.price_change_percentage_7d_in_currency.eur)).toFixed(2)} <span>${marketData.price_change_percentage_7d_in_currency.eur > 0 ? "+": ""}${marketData.price_change_percentage_7d_in_currency.eur.toFixed(2)}%</span>`;
+        
+        priceOneWeek.lastChild.style.color = marketData.price_change_percentage_7d_in_currency.eur > 0 ? "green" : "red";
 
-        priceOneMonth.innerHTML = `€ ${((response.data.market_data.current_price.eur * 100) / (100 + response.data.market_data.price_change_percentage_30d_in_currency.eur)).toFixed(2)} <span>${response.data.market_data.price_change_percentage_30d_in_currency.eur.toFixed(2)}%</span>`;
+        priceOneMonth.innerHTML = `€ ${((marketData.current_price.eur * 100) / (100 + marketData.price_change_percentage_30d_in_currency.eur)).toFixed(2)} <span>${marketData.price_change_percentage_30d_in_currency.eur > 0 ? "+": ""}${marketData.price_change_percentage_30d_in_currency.eur.toFixed(2)}%</span>`;
 
-        price200days.innerHTML = `€ ${((response.data.market_data.current_price.eur * 100) / (100 + response.data.market_data.price_change_percentage_200d_in_currency.eur)).toFixed(2)} <span>${response.data.market_data.price_change_percentage_200d_in_currency.eur.toFixed(2)}%</span>`;
+        priceOneMonth.lastChild.style.color = marketData.price_change_percentage_30d_in_currency.eur > 0 ? "green" : "red";
+
+        price200days.innerHTML = `€ ${((marketData.current_price.eur * 100) / (100 + marketData.price_change_percentage_200d_in_currency.eur)).toFixed(2)} <span>${marketData.price_change_percentage_200d_in_currency.eur > 0 ? "+": ""}${marketData.price_change_percentage_200d_in_currency.eur.toFixed(2)}%</span>`;
+
+        price200days.lastChild.style.color = marketData.price_change_percentage_200d_in_currency.eur > 0 ? "green" : "red";
 
         // Gestion des entrées des formulaires
 
